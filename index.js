46ac0eaa55146239a2f9b5a18ce9dcd691fb89d7
Bartlomiej Marek <bartlomiejmarek93@gmail.com>
2019-02-16 00:11:39 +0100

    Axios library replaced with vanilla js for server POST.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
-// Import the axios library, to make HTTP requests
-const axios = require("axios");
 
 // This is the client ID and client secret that you obtained
 // while registering the application
-const clientID = "32572";
-const clientSecret = "5e338c16d1f1a398f046296384bf06812073d390";
-const grantTypeAuthorization = "authorization_code";
-const grantTypeRefresh = "refresh_token";
+const CLIENT_ID = "32572";
+const CLIENT_SECRET = "5e338c16d1f1a398f046296384bf06812073d390";
+const GRANT_TYPE_AUTHORIZATION = "authorization_code";
+const GRANT_TYPE_REFRESH = "refresh_token";
 
 const app = express();
 app.use(cors());
@@ -22,33 +20,61 @@ app.use(function(req, res, next) {
 
 app.get("/oauth/redirect", (req, res) => {
   const requestToken = req.query.code;
-  console.log("GET oauth/redirect");
-  axios({
-    method: "POST",
-    url: `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&grant_type="${grantTypeAuthorization}"`,
-    headers: {
-      accept: "application/json"
-    }
-  }).then(response => {
-    const accessToken = response.data.access_token;
-    const refreshToken = response.data.refresh_token;
-    const expiresAt = response.data.expires_at;
-    const athleteId = response.data.athlete.id;
-    const state = response.data.state;
-    // redirect the user to the welcome page, along with the access token
-    res.redirect(
-      `/welcome.html?access_token=${accessToken}&athlete=${athleteId}&refresh_token=${refreshToken}&expires_at=${expiresAt}`
-    );
-  });
-});
+  console.log("GET oauth/redirect, request token=", requestToken);
 
-// app.post('/meals', (req, res) => {
-//   let meal = req.body
-//   meal.id = Date.now()
-//   meals.push(meal)
-//   return res.send(meal)
-// })
+  var requestBody = {
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    grant_type: GRANT_TYPE_AUTHORIZATION,
+    code: requestToken
+  };
+  var response = request(
+    "POST",
+    "https://www.strava.com/oauth/token",
+    false,
+    requestBody
+  );
+
+  const accessToken = response.access_token;
+  const refreshToken = response.refresh_token;
+  const expiresAt = response.expires_at;
+  const athleteId = response.athlete.id;
+  const state = response.state;
+  // redirect the user to the welcome page, along with the access token
+
+  res.redirect(
+    `/welcome.html?access_token=${accessToken}&athlete=${athleteId}&refresh_token=${refreshToken}&expires_at=${expiresAt}`
+  );
+});
 
 app.listen(5000, () => {
   console.log("App listening on port 5000!");
 });
+
+function request(method, url, bearer = true, data = null) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Accept", "application/json");
+    if (bearer) {
+      xhr.setRequestHeader(
+        "Authorization",
+        `${token.tokenType} ${token.accessToken}`
+      );
+    }
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        return resolve(JSON.parse(xhr.responseText || "{}"));
+      } else {
+        console.log(`Request failed with status ${xhr.status}`);
+        return reject(new Error(`Request failed with status ${xhr.status}`));
+      }
+    };
+    if (data) {
+      xhr.send(JSON.stringify(data));
+    } else {
+      xhr.send();
+    }
+  });
+}
